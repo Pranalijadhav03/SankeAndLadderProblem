59755f0bf2e5639d42dc9125e5e140c13121381d
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-04-28 17:10:33 +0530

    Added: Snake & ladder problem 1

---
+++ DiceBetween1to6/DiceBetween1to6/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random obj = new Random();
+int dice = obj.Next(1,7);
+Console.WriteLine("dice number is:" + dice);
+Console.ReadKey();
+
